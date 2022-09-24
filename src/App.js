6b832e5330d4c183c69da51970b1b7ae0eaa2d8f
Joel Nii Bortey Alabi <joelalabi45@gmail.com>
2022-09-24 15:29:10 +0000

    coded real work

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import DSCN6494 from "./DSCN6494.png"
+import DSCN6508 from "./DSCN6508.png"
+import IMG_1527 from "./IMG_1527.png"
+import IMG_1528 from "./IMG_1528.png"
+import IMG_6316 from "./IMG_6316.JPG"
+import IMG_6676 from "./IMG_6676.JPG"
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="bday">HAPPY BIRTHDAY</div>
+      <div className="name">JOEL ALABI</div>
+
+      <div className="wrapper">
+        <div className="img-area">
+        <div className="single-img">
+          <div className="single-img img-one">
+            <img src={DSCN6494} alt=" "/>
+          </div> 
+          <div className="single-img">
+          <div className="single-img img-two">
+            <img src={DSCN6508} alt=" "/>
+            </div>
+          </div>
+          <div className="single-img">
+          <div className="single-img img-three">
+            <img src={IMG_1527} alt=" "/>
+          </div>
+          </div>
+        
+        </div>
+        </div>
+      </div>
+    </>
   );
 }
 
